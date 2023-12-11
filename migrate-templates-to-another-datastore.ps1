c038b14be4d76d7f883d4e6d8a745d70c77ee624
Zero3Dash <48377222+Zero3Dash@users.noreply.github.com>
2023-12-11 13:25:17 +0000

    Update migrate-templates-to-another-datastore.ps1

--- migrate-templates-to-another-datastore.ps1
+++ migrate-templates-to-another-datastore.ps1
@@ -1,10 +1,16 @@
-$DS = "[[Target-DataStore-Name]]"
-$template_list = get-template
+# Define variables
+$sourceDatastore = "Source Datastore"
+$destinationDatastore = "Destination Datastore"
+
+# Get templates on the source datastore
+$template_list = Get-Datastore $sourceDatastore | Get-Template
+
+# Move the template to the new location
 foreach ($template in $template_list) {
     Write-Output "convert template  $template to vm"
     Get-Template $template | Set-Template -ToVM -Confirm:$false
-    Write-Output "Move-vm to Datastore $DS"
-    get-vm $template | Move-vm -Datastore $DS
+    Write-Output "Move-vm to Datastore $destinationDatastore"
+    get-vm $template | Move-vm -Datastore $destinationDatastore
     Write-Output "convert $template vm to template"
     Get-Vm $template | Set-Vm -ToTemplate -Confirm:$false
     Write-Output "done"
