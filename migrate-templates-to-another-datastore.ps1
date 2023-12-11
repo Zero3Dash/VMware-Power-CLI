a202b67aa80ae32d1b60f5dea47b65503f910179
Zero3Dash <48377222+Zero3Dash@users.noreply.github.com>
2023-12-11 16:05:57 +0000

    Update migrate-templates-to-another-datastore.ps1

--- migrate-templates-to-another-datastore.ps1
+++ migrate-templates-to-another-datastore.ps1
@@ -1,8 +1,21 @@
-#import power-cli module
+# Turn off certificate warning
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+
+# Install power-cli
+Install-Module -Name VMware.PowerCLI -Scope AllUsers -SkipPublisherCheck
+
+# Import power-cli module
 Import-Module VMware.PowerCLI
 
+# Define variables
+$sourceDatastore = "Source Datastore"
+$destinationDatastore = "Destination Datastore"
+$vCenterServer = "vcenter.domain.local"
+$vcenterUsername = "vcenterUsername"
+$vCenterPassword = "vCenterPassword"
+
 # Connect to vCenter Server
-Connect-VIServer -Server YourVCenterServer -User YourUsername -Password YourPassword
+Connect-VIServer -Server $vCenterServer -User $vcenterUsername -Password $vCenterPassword
 
 # Define variables
 $sourceDatastore = "Source Datastore"
@@ -13,11 +26,11 @@ $template_list = Get-Datastore $sourceDatastore | Get-Template
 
 # Move the template to the new location
 foreach ($template in $template_list) {
-    Write-Output "convert template  $template to vm"
+    Write-Output "Convert Template $template to VM"
     Get-Template $template | Set-Template -ToVM -Confirm:$false
-    Write-Output "Move-vm to Datastore $destinationDatastore"
+    Write-Output "Moving VM to Datastore $destinationDatastore"
     get-vm $template | Move-vm -Datastore $destinationDatastore
-    Write-Output "convert $template vm to template"
+    Write-Output "Convert $template VM to Template"
     Get-Vm $template | Set-Vm -ToTemplate -Confirm:$false
-    Write-Output "done"
+    Write-Output "$template Completed"
  }
